f940f17c8bb947e77fb6d38ccefd299a76257828
Michelle <michelledantasluiz@gmail.com>
2022-03-04 10:19:02 -0300

    Refactor catch at class Program

--- src/main/java/application/Program.java
+++ src/main/java/application/Program.java
@@ -39,11 +39,7 @@ public class Program {
                     captured.add(capturedPiece);
                 }
             }
-            catch (ChessException e) {
-                System.out.println(e.getMessage());
-                sc.nextLine();
-            }
-            catch (InputMismatchException e) {
+            catch (ChessException | InputMismatchException e) {
                 System.out.println(e.getMessage());
                 sc.nextLine();
             }
